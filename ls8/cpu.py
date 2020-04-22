190542526618191e2522695a23888231b37c2d97
TimTree <TimTree@users.noreply.github.com>
2020-04-22 15:38:15 -0700

    Beautify run() loop

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,11 +20,48 @@ class CPU:
         self.pc = 0
         self.program_filename = sys.argv[1]
 
-        # Stack pointer location is in position 7 of the register as per the spec
+        self.branchtable = {}
+        self.branchtable[HLT] = self.handle_hlt
+        self.branchtable[LDI] = self.handle_ldi
+        self.branchtable[PRN] = self.handle_prn
+        self.branchtable[MUL] = self.handle_mul
+        self.branchtable[PUSH] = self.handle_push
+        self.branchtable[POP] = self.handle_pop
+
+        # Stack pointer location is in position 7 of the register per the spec
         self.SP = 7
 
         # The stack by default points to address 0xF4
         self.reg[self.SP] = 0xF4
+
+    def handle_hlt(self):
+        sys.exit()
+
+    def handle_ldi(self):
+        reg_num = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+        self.reg[reg_num] = value
+        self.pc += 3
+
+    def handle_prn(self):
+        print(self.reg[self.ram_read(self.pc + 1)])
+        self.pc += 2
+
+    def handle_mul(self):
+        value0 = self.reg[self.ram_read(self.pc + 1)]
+        value1 = self.reg[self.ram_read(self.pc + 2)]
+        self.reg[self.ram_read(self.pc + 1)] = value0 * value1
+        self.pc += 3
+
+    def handle_push(self):
+        self.reg[self.SP] -= 1  # Decrement SP
+        self.ram_write(self.reg[self.SP], self.reg[self.ram_read(self.pc + 1)])
+        self.pc += 2
+
+    def handle_pop(self):
+        self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.reg[self.SP])
+        self.reg[self.SP] += 1  # Increment SP
+        self.pc += 2
 
     def load(self):
         """Load a program into memory."""
@@ -43,13 +80,12 @@ class CPU:
                 self.ram_write(address, int(line, 2))
                 address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -61,8 +97,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -82,40 +118,10 @@ class CPU:
     def run(self):
         """Run the CPU."""
 
-        running = True
-
-        while running:
+        while True:
             ir = self.ram_read(self.pc)
-
-            if ir == LDI:
-                reg_num = self.ram_read(self.pc + 1)
-                value = self.ram_read(self.pc + 2)
-                self.reg[reg_num] = value
-                self.pc += 3
-
-            elif ir == PRN:
-                print(self.reg[self.ram_read(self.pc + 1)])
-                self.pc += 2
-
-            elif ir == MUL:
-                value0 = self.reg[self.ram_read(self.pc + 1)]
-                value1 = self.reg[self.ram_read(self.pc + 2)]
-                self.reg[self.ram_read(self.pc + 1)] = value0 * value1
-                self.pc += 3
-
-            elif ir == HLT:
-                running = False
-
-            elif ir == PUSH:
-                self.reg[self.SP] -= 1  # Decrement SP
-                self.ram_write(self.reg[self.SP], self.reg[self.ram_read(self.pc + 1)])
-                self.pc += 2
-
-            elif ir == POP:
-                self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.reg[self.SP])
-                self.reg[self.SP] += 1  # Increment SP
-                self.pc += 2
-
-            else:
+            try:
+                self.branchtable[ir]()
+            except KeyError:
                 print("Unknown instruction")
-                running = False
+                self.handle_hlt()
